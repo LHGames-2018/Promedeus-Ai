85fc29135a8fab6fa754d990b5603231d1423fbf
Christophe Vauclair <cfvauclair@gmail.com>
2018-10-06 11:38:58 -0400

    Add find_closest_resource and find_home

--- bot/bot.py
+++ bot/bot.py
@@ -20,10 +20,32 @@ class Bot:
         """
 
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        return create_move_action(Point(1, 0))
+		if(playerInfo.carriedResources < playerInfo.carryingCapacity)
+			dest = find_closest_resource(gameMap)
+		else
+			dest = find_home(gameMap)
+
+		return create_move_action(Point(1, 0))
 
     def after_turn(self):
         """
         Gets called after executeTurn
         """
         pass
+
+
+	def find_closest_resource(self, gameMap):
+		# Find closest resource
+		for x in range (0,20):
+			for y in range (0,20):
+				if (gameMap.getTileAt(x,y) == Resource)
+					return = (x,y)
+
+
+	def find_home(self, gameMap):
+		# Find closest resource
+		for x in range (0,20):
+			for y in range (0,20):
+				if (gameMap.getTileAt(x,y) == House)
+					return = (x,y)
+		
