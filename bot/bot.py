6137406d1ba3c27b0106399fc3570a2f9ab1d70b
Unknown <mbelanger.poly@gmail.com>
2018-10-06 13:26:58 -0400

    yp

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,5 @@
 from helper import *
-from StorageHelper import *
+
 
 class Bot:
     def __init__(self):
@@ -11,10 +11,10 @@ class Bot:
             :param playerInfo: Your bot's current state.
         """
         self.PlayerInfo = playerInfo
-        if read("isInit") == None:
-            write("isInit", True) 
-            write("bptr", [Point(-1,0),Point(-1,0),Point(-1,0),Point(-1,0)])
-            write("posInPath") = 0
+        if StorageHelper.read("isInit") is None:
+            StorageHelper.write("isInit", True)
+            StorageHelper.write("bptr", [Point(0, 1), Point(0, 1),Point(0, 1), Point(0, 1)])
+            StorageHelper.write("posInPath", 0)
 
     def execute_turn(self, gameMap, visiblePlayers):
         """
@@ -23,18 +23,19 @@ class Bot:
             :param visiblePlayers:  The list of visible players.
         """
 
-        if read("posInPath") < len(read("bptr")):
-            return create_move_action(read("bptr"))
-            write("posInPath", read("posInPath") + 1)
-            
-        # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        """ 
-        if(playerInfo.carriedResources < playerInfo.carryingCapacity)
-			dest = find_closest_resource(gameMap)
-		else
-			dest = find_home(gameMap)
+        if StorageHelper.read("posInPath") < len(StorageHelper.read("bptr")):
+            StorageHelper.write("posInPath", StorageHelper.read("posInPath") + 1)
+            return create_move_action(StorageHelper.read("bptr"))
 
-		return create_move_action(Point(1, 0))
+
+        # Write your bot here. Use functions from aiHelper to instantiate your actions.
+        """
+        if(playerInfo.carriedResources < playerInfo.carryingCapacity)
+            dest = find_closest_resource(gameMap)
+        else
+            dest = find_home(gameMap)
+
+        return create_move_action(Point(1, 0))
         """
 
     def after_turn(self):
@@ -45,36 +46,25 @@ class Bot:
 
     def find_closest_resource(self, gameMap):
         # Find closest resource
-        for x in range (0, 20):
-            for y in range (0, 20):
+        for x in range(0, 20):
+            for y in range(0, 20):
                 if gameMap.getTileAt(x, y) == TileContent.Resource:
                     return (x, y)
 
     def find_home(self, gameMap):
         # Find closest resource
-        for x in range (0, 20):
-            for y in range (0, 20):
+        for x in range(0, 20):
+            for y in range(0, 20):
                 if gameMap.getTileAt(x, y) == TileContent.House:
                     return (x, y)
 
-
-	def find_home(self, gameMap):
-		# Find closest resource
-		for x in range (0,20):
-			for y in range (0,20):
-				if (gameMap.getTileAt(x,y) == House)
-					return = (x,y)
-
-    def get_resource(self, gameMap)
-        #Collect resources
-        if gameMap.getTileAt(1,0) == Resource
-            return create_collect_action(Point(1,0))
-        else if gameMap.getTileAt(0,1) == Resource
-            return create_collect_action(Point(0,1))
-        else if gameMap.getTileAt(0,-1) == Resource
-            return create_collect_action(Point(0,-1))
-        else if gameMap.getTileAt(-1,0) == Resource
-            return create_collect_action(Point(-1,0))
-
-
-		
+    def get_resource(self, gameMap):
+        # Collect resources
+        if gameMap.getTileAt(1, 0) == TileContent.Resource:
+            return create_collect_action(Point(1, 0))
+        elif gameMap.getTileAt(0, 1) == TileContent.Resource:
+            return create_collect_action(Point(0, 1))
+        elif gameMap.getTileAt(0, -1) == TileContent.Resource:
+            return create_collect_action(Point(0, -1))
+        elif gameMap.getTileAt(-1, 0) == TileContent.Resource:
+            return create_collect_action(Point(-1, 0))
