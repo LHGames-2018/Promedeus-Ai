c929f4b13850671a489159bb228923c2ab89a615
Christophe Vauclair <cfvauclair@gmail.com>
2018-10-06 15:12:46 -0400

    Not so smart AI

--- bot/bot.py
+++ bot/bot.py
@@ -1,32 +1,59 @@
 from helper import *
 import math
+import queue
+from random import randint
 
-class Bot:
+class Bot: 
     def __init__(self):
         pass
 
     def before_turn(self, playerInfo):
         """
-        Gets called before ExecuteTurn. This is where you get your bot's state.
-            :param playerInfo: Your bot's current state.
+		Gets called before ExecuteTurn. This is where you get your bot's state.
+		    :param playerInfo: Your bot's current state.
         """
         self.PlayerInfo = playerInfo
 
     def execute_turn(self, gameMap, visiblePlayers):
         """
-        This is where you decide what action to take.
-            :param gameMap: The gamemap.
-            :param visiblePlayers:  The list of visible players.
+		This is where you decide what action to take.
+			:param gameMap: The gamemap.
+			:param visiblePlayers:  The list of visible players.
         """
 
         # Write your bot here. Use functions from aiHelper to instantiate your actions.
+        dropoff = False
         if self.PlayerInfo.CarriedResources < self.PlayerInfo.CarryingCapacity:
-            dest = find_closest(gameMap, (11,11), Resource)
+            dest = self.find_closest(gameMap, self.PlayerInfo.Position, TileContent.Resource)
         else:
-            dest = find_closest(gameMap, (11,11), Home)
+            dropoff = True
+            dest = self.find_closest(gameMap, self.PlayerInfo.Position, TileContent.House)
 
+        print(self.PlayerInfo.Position)
         print(dest)
-        return create_move_action(Point(1, 0))
+
+        if dest == None:
+            r = randint(0,3)
+            if r == 0:
+                return create_move_action(Point(1, 0))
+            elif r == 1:
+                return create_move_action(Point(-1, 0))
+            elif r == 2:
+                return create_move_action(Point(0, 1))
+            elif r == 3:
+                return create_move_action(Point(0, -1))
+         
+        if Point.Distance(dest, self.PlayerInfo.Position) == 1 and not dropoff:
+            return create_collect_action(dest - self.PlayerInfo.Position)
+ 
+        if dest.x - self.PlayerInfo.Position.x < 0:
+            return create_move_action(Point(-1, 0))
+        elif dest.x - self.PlayerInfo.Position.x > 0:
+            return create_move_action(Point(1, 0))
+        elif dest.y - self.PlayerInfo.Position.y < 0:
+            return create_move_action(Point(0, -1))
+        elif dest.y - self.PlayerInfo.Position.y > 0:
+            return create_move_action(Point(0, 1))
 
     def after_turn(self):
         """
@@ -34,31 +61,30 @@ class Bot:
         """
         pass
 
-    def find_closest(gameMap, start, tileType):
-        frontier = Queue()
-        frontier.put(start)
+    def find_closest(self, gameMap, start, tileType):
+        points = []
+        for x in range (start.x - 10, start.x + 10):
+            for y in range (start.y - 10, start.y + 10):
+                if gameMap.getTileAt(Point(x,y)) == tileType:
+                    points.append(Point(x,y))
 
-        came_from = {}
-        came_from[start] = None
+        if not points:
+            return None
+        
+        closest = points[0]
+        for p in points:
+            if Point.Distance(start, p) < Point.Distance(start, closest):
+                closest = p
 
-        # While there are tiles to search
-        while not frontier.empty():
-            current = frontier.get()
+        return closest
 
-            # Check if tile is what we are looking for
-            if gameMap.getTileAt(current) == tileType:
-                break
-
-            (x,y) = current
-            neighbors = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
-            neighbors = filter(lambda x,y : x >= 0 and x <= 20 and y >= 0 and y <= 20, neighbors)
-
-            for n in neighbors:
-                if n not in came_from:
-                    frontier.put(n)
-                    came_from[n] = current
-
-        return came_from
+    def find_resource(self, gameMap):
+        for x in range (0,20):
+            for y in range (0,20):
+                if gameMap.getTileAt(Point(x,y)) == TileContent.Resource:
+                    return (x,y)
+        
+        return (10,10)
 
     def map_to_graph(gameMap):
         vertices = []
@@ -68,7 +94,7 @@ class Bot:
     def generate_path(self, gameMap, dest):
         # Using A*
 
-        start = (11,11)
+        start = (10,10)
         nodes = []
         for x in range (0,20):
             for y in range (0,20):
